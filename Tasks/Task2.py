fa4eb6246b4b002f0a02ac2f80a14f337692fe6c
Тихонова Ирина Геннадьевна <sondrafinchley@mail.ru>
2022-05-27 15:28:55 +0300

    Task2_done

---
+++ Tasks/Task2.py
@@ -0,0 +1,11 @@
+def wave(text: str) -> str:
+    list_ = []
+    for i in range(0, len(text)):
+        if not text[i] == " ":
+            result = text[:i] + text[i].upper() + text[i+1:]
+            list_.append(result)
+    return "\n".join(list_)
+
+
+if __name__ == '__main__':
+    print(wave("hello world"))
