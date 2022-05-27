3ff6c7f97c16fae8d1c553818476d6d03d14b8e9
Тихонова Ирина Геннадьевна <sondrafinchley@mail.ru>
2022-05-27 15:27:15 +0300

    Task1_done

---
+++ Tasks/Task1.py
@@ -0,0 +1,5 @@
+def binary_to_decimal(list_of_numbers : list) -> int:
+    return int("".join([str(x) for x in list_of_numbers]), base=2)
+
+if __name__ == '__main__':
+    print(binary_to_decimal([0,1,1,0]))
